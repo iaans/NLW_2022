ec2d5b48c86f0a1cd92fdf9e1ae79323802bc281
Ian Augusto Silva <vntinsi@Ian-Augusto-Silva.local>
2022-05-02 23:29:23 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+interface ButtonProps {
+  text: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.text} </button>;
+}
+
+function App() {
+  return (
+    <div>
+      <Button text="Enviar" />
+      <Button text="Ok" />
+    </div>
+  );
+}
+
+export default App;
